c50b9f18b6346f828c8a35f2fe4ea7b81d583063
sardor <136901570+salimovone@users.noreply.github.com>
2024-04-17 17:48:11 +0500

    new progress

--- src/app/skills/page.jsx
+++ src/app/skills/page.jsx
@@ -1,31 +1,44 @@
-"use client"
-import { useRouter } from 'next/navigation';
-import React, { useEffect } from 'react';
+"use client";
+import ProgressBar from "@/components/Progress";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 const Skills = () => {
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
     const handleWheel = (event) => {
       const delta = event.deltaY;
       event.preventDefault();
-      if(delta>0){
-        router.push("/experience")
+      if (delta > 0) {
+        router.push("/experience");
       }
-      if(delta<0){
-        router.push("/me")
+      if (delta < 0) {
+        router.push("/me");
       }
     };
 
-    document.addEventListener('wheel', handleWheel);
+    document.addEventListener("wheel", handleWheel);
     return () => {
-      document.removeEventListener('wheel', handleWheel);
+      document.removeEventListener("wheel", handleWheel);
     };
-  }, [])
+  }, []);
 
   return (
-    <div className="h-screen w-full text-white flex justify-center items-center">
-      Skills
+    <div className="h-screen w-full text-white flex items-center px-5">
+      <div className="w-full">
+        <ProgressBar
+          duration={5000}
+          name={"HTML"}
+          percent={80}
+        />
+        <ProgressBar
+          duration={5000}
+          name={"JS"}
+          percent={90}
+          color={"yellow"}
+        />
+      </div>
     </div>
   );
 };
