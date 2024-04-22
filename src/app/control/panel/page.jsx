688abf357f4210ec8a9e65ca208431422f1029d4
sardor salimov <salimovone@gmail.com>
2024-04-23 03:38:58 +0500

    chala product

--- src/app/control/panel/page.jsx
+++ src/app/control/panel/page.jsx
@@ -4,11 +4,26 @@ import React, { useState } from "react";
 import styles from "./controlPanel.module.css";
 import { pages } from "./helper";
 import Game from "@/components/control/Game";
+import Auth from "@/components/Auth";
+import { signOut } from "firebase/auth";
+import { auth } from "@/firebaseConfig";
 
 const ControlPanel = () => {
   const [page, setPage] = useState(<Game />);
   const [active, setActive] = useState(0);
-  return (
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState("");
+
+  const logChange = (signedUser) => {
+    setUser(signedUser);
+    setIsLoggedIn(true);
+  };
+
+  const logout = async () => {
+    await signOut(auth);
+  };
+
+  return isLoggedIn ? (
     <div
       className={
         "text-white absolute z-[999] top-0 left-0 w-screen h-screen grid grid-cols-12 grid-rows-12 " +
@@ -40,6 +55,8 @@ const ControlPanel = () => {
       </div>
 
       <div className="border-b mt-5 col-span-12 h-fit border-[val(--main-color)] md:hidden row-span-1">
+       
+
         <ul className="w-full px-5 grid grid-cols-3 gap-x-2">
           {pages.map((item, index) => (
             <li
@@ -64,8 +81,12 @@ const ControlPanel = () => {
       </div>
 
       {/* second area */}
-      <div className="md:col-span-10 col-span-12 px-5 pt-3 max-md:mt-16 max-md:row-span-11">{page}</div>
+      <div className="md:col-span-10 col-span-12 px-5 pt-3 max-md:mt-16 max-md:row-span-11">
+        {page}
+      </div>
     </div>
+  ) : (
+    <Auth func={logChange} />
   );
 };
 
